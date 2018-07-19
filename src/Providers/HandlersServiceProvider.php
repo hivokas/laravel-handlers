2ac9731165d7b1a94b0a93459970d0127ae248fb
Ilya Sakovich <me@hivokas.com>
2018-07-20 01:50:55 +0300

    Created HandlersServiceProvider

---
+++ src/Providers/HandlersServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hivokas\LaravelHandlers\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HandlersServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
