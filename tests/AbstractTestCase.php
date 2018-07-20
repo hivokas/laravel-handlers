e6bd8ed721d4f30bc0a0ffa0ac86ffaba669fea7
Ilya Sakovich <me@hivokas.com>
2018-07-20 17:01:20 +0300

    Added some helpful stuff to AbstractTestCase

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -2,11 +2,29 @@
 
 namespace Hivokas\LaravelHandlers\Tests;
 
+use Illuminate\Filesystem\Filesystem;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use Hivokas\LaravelHandlers\Providers\HandlersServiceProvider;
 
 abstract class AbstractTestCase extends OrchestraTestCase
 {
+    /**
+     * @var Filesystem
+     */
+    protected $files;
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application   $app
+     *
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $this->files = new Filesystem();
+    }
+
     /**
      * Get package providers.
      *
@@ -18,5 +36,33 @@ abstract class AbstractTestCase extends OrchestraTestCase
         return [
             HandlersServiceProvider::class,
         ];
+    }
+
+    /**
+     * Make the directory.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    protected function makeDirectory(string $path): string
+    {
+        if (! $this->files->isDirectory(dirname($path))) {
+            $this->files->makeDirectory(dirname($path), 0777, true, true);
+        }
+
+        return $path;
+    }
+
+    /**
+     * Put contents to file.
+     *
+     * @param string $path
+     * @param string $contents
+     * @param bool $lock
+     */
+    protected function forceFilePutContents(string $path, string $contents, bool $lock = false): void
+    {
+        $this->makeDirectory($path);
+        $this->files->put($path, $contents, $lock);
     }
 }
